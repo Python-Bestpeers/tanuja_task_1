1cffa40becebce87c6b7f827de686d15e8215078
tanuja009 <tanujapatidar01@gmail.com>
2024-12-12 18:48:41 +0530

    changes saved

--- task_management_system/settings.py
+++ task_management_system/settings.py
@@ -141,5 +141,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'tapatidar@bestpeers.com'
-EMAIL_HOST_PASSWORD = 'ugdc izzp cugj akjl'
+EMAIL_HOST_USER =os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
